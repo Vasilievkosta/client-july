db9f6eb7c1671e2612a900e56761af8e3e6e9071
Vasilievkosta <vasilievkosta@gmail.com>
2022-07-30 20:42:00 +0300

    added jwt userAPI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Context } from './index';
 import AppRouter from './components/AppRouter';
 import NavBar from './components/NavBar';
+import { check } from './http/userAPI';
 
-function App() {
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    check().then(data => {
+      user.setUser(true);
+      user.setIsAuth(true);
+    }).finally(() => setLoading(false))
+  }, []);
+
+  // if (loading) {
+  //   return <Spinner/>
+  // }
+
   return (
     <BrowserRouter>
       <NavBar />
@@ -11,6 +28,6 @@ function App() {
     </BrowserRouter>
 
   );
-}
+})
 
 export default App;
